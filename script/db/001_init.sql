2bd80060b5235010ce1d2416d7e84cb89560d9d6
Lamine Sow <lamine.2004pro.sow@gmail.com>
2026-02-05 19:46:43 +0100

    Rebuild

--- script/db/001_init.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists formations (
-  id serial primary key,
-  nom text not null,
-  created_at timestamptz not null default now()
-);
-
-insert into formations (nom) values ('Formation exemple');
-
