d7e143f93205b9ba0022428b5e58a901b1afc940
birane18 <mpapebirane@gmail.com>
2026-04-08 14:42:30 +0200

    feat(annees): met a jour le controller annees

--- backend-nest/src/modules/annees/annees.controller.ts
+++ backend-nest/src/modules/annees/annees.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { ROLE_IDS } from '../../auth/roles.constants';
 import { Roles } from '../../common/decorators/roles.decorator';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import type { CurrentUser as CurrentUserType } from '../../common/types/current-user';
 import { AnneesService } from './annees.service';
 import { YearsListQueryDto } from './dto/years-list-query.dto';
 import { CloneYearDto } from './dto/clone-year.dto';
@@ -12,8 +14,11 @@ export class AnneesController {
 
   @Get()
   @Roles(...Object.values(ROLE_IDS))
-  async list(@Query() query: YearsListQueryDto) {
-    const items = await this.anneesService.list(query.statut);
+  async list(
+    @CurrentUser() user: CurrentUserType,
+    @Query() query: YearsListQueryDto,
+  ) {
+    const items = await this.anneesService.listForUser(user, query.statut);
     return { items };
   }
 
@@ -39,5 +44,11 @@ export class AnneesController {
   ) {
     const year = await this.anneesService.updateStatus(id, payload.statut);
     return { year };
+  }
+
+  @Delete(':id')
+  @Roles(ROLE_IDS.SERVICES_CENTRAUX)
+  async remove(@Param('id') id: string) {
+    return this.anneesService.deleteYear(id);
   }
 }
